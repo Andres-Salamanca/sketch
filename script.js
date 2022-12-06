08fbf9cb4e0fee82221e6b9c9a79ec2b8b611f56
Andres-Salamanca <andrealebarbaritos@gmail.com>
2022-12-06 14:54:26 -0500

    add slider and end javascript dynamicaly create grid

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 
-div = document.querySelector('.container');
-div.style.cssText="display: grid; grid-template-columns: repeat(16,1fr);";
-for(i=0;i<256;i++){
-    //result.textContent
-    squares= document.createElement('div');
-    squares.classList.add(`grid`);
-    div.appendChild(squares);
-    /*for(j=0;j<16;j++){
-        gridcolum=document.querySelector(`.grid${i}`);
-        rows= document.createElement('div');
-        rows.classList.add(`column${j}`);
-        gridcolum.appendChild(rows);
-    }*/
+let div = document.querySelector('.container');
+let slider = document.querySelector('.slider');
+removeElements();
+createBoard();
+slider.onchange =function(){
+    removeElements()
+    createBoard();
+};
+function createBoard(){
+    div.style.cssText=`display: grid; grid-template-columns: repeat(${slider.value},1fr);`;
+    for(i=0;i<(slider.value*slider.value);i++){//need to change the for maximun depending on number of squares
+        let squares= document.createElement('div');
+        squares.classList.add(`grid`);
+        div.appendChild(squares);
+        
+    }
+}
+function removeElements() {
+    const list = document.querySelector(".container");
+    while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+    }
 }
